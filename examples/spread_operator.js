f82831983adb2546b78478ce2a8b1fa222348a31
graceFor <tbgusrud12@gmail.com>
2020-08-11 13:04:31 +0900

    [1.4 Spread Operator]

--- examples/spread_operator.js
+++ examples/spread_operator.js
@@ -10,3 +10,17 @@ console.log(allDay); // [ [ 'Mon', 'Tues', 'Wed' ], [ 'Thu', 'Fri', 'Sat' ] ]
 //Spread Operator
 const allDay_2 = [...days, ...otherdays, "Sun"];
 console.log(allDay_2); //[ 'Mon', 'Tues', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+
+//object
+
+const ob = {
+  first: "hi",
+  second: "hello",
+};
+
+const ab = {
+  third: "bye",
+};
+
+const add = { ...ob, ...ab };
+console.log(add); // { first: 'hi', second: 'hello', third: 'bye' }
