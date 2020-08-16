fece7d4e83cc00efd73cd27edc5f8970bfd68045
graceFor <tbgusrud12@gmail.com>
2020-08-17 01:24:51 +0900

    [setting]

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
-//import React from 'react';
-//import logo from './logo.svg';
-// import './App.css';
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return <div className="App" />;
+  }
+}
+
+export default App;
