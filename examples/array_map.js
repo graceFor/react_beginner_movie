b283e87c3fdd7bb58f787a6d22e8d8989e55599d
graceFor <tbgusrud12@gmail.com>
2020-08-17 00:28:29 +0900

    [1.6 Array map]

--- examples/array_map.js
+++ examples/array_map.js
@@ -5,3 +5,10 @@ const days = ["Mon", "Tue", "Wed", "Thurs", "Fri"];
 const smilingDays = days.map((a, index) => `#${index + 1} ${a}`);
 
 console.log(smilingDays);
+
+// Another method
+const addNumber = (day, index) => `#${index + 1} ${day}`;
+
+const NumDays = days.map(addNumber);
+
+console.log(NumDays);
